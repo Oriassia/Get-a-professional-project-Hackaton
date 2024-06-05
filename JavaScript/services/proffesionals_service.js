99eae041e5b6e833416b2694196cde6d64ba714f
JoniUzan <joniuzan33@gmail.com>
2024-06-05 21:25:53 +0300

    joni v1.0

--- JavaScript/services/proffesionals_service.js
+++ JavaScript/services/proffesionals_service.js
@@ -1,36 +1,93 @@
 // const proffesionalsData = await axios.get(url)
+const baseURL = "http://localhost:8002/professionals";
+const elementCardsContainer = document.querySelector(
+  ".proffesional-card-container"
+);
 
-const params = new URLSearchParams(window.location.search)
-const specializationFilterValue = params.get('specialization')
-const servicaAreaFilterValue = params.get('servicearea')
-const ratingFilterValue = params.get('rating')
+let proffesionalsArray;
 
-window.onload = proffesionalsPageInit(...proffesionalsData)
-
-function proffesionalsPageInit(data) {
-    let filteredData = [...data]; // Make a copy of the data array
-
-    if (specializationFilterValue) {
-        filteredData = filteredData.filter(item => item.specialization.includes(specializationFilterValue));
-    }
-
-    if (servicaAreaFilterValue) {
-        filteredData = filteredData.filter(item => item.serviceArea.includes(servicaAreaFilterValue));
-    }
-
-    if (ratingFilterValue) {
-        filteredData = filterByRating(ratingFilterValue, filteredData);
-    }
-
-    return filteredData;
+init();
+function init() {
+  proffesionalsGet();
+  setTimeout(() => {
+    renderProffesionalsCards(proffesionalsPageInit(proffesionalsArray));
+    console.log(proffesionalsArray);
+  }, 1000);
 }
 
+async function proffesionalsGet() {
+  const proffesionals = await axios.get(baseURL);
+  proffesionalsArray = proffesionals.data;
+}
+function renderProffesionalsCards(array) {
+  elementCardsContainer.innerHTML = "";
+  for (const obj of array) {
+    const card = `
+    <a href = "http://127.0.0.1:5500/HTML/proffesionalDetails.html?id=${obj.id}">
+    <div class = "proffesional-card" >
+    <img src="" alt=""></img>
+    <p>${obj.name}</p>
+    <p>${obj.specialization}</p>
+    <p>${obj.serviceArea}</p>
+    
+    </div>
+    </a>
+      `;
 
-function filterByRating(ratingFilterValue, dataArray){
-        return dataArray.filter(item =>{
-        const starsSum = item.rating.totalStars
-        const usersWhoRatedSum = item.rating.usersWhoRated
-        const ratingSum = (starsSum / usersWhoRatedSum).toFixed(1)
-       return ratingSum >= ratingFilterValue
-    })
+    elementCardsContainer.innerHTML += card;
+  }
+}
+
+function updateUrlByFilter(ev) {
+  ev.preventDefault();
+  console.log("hiiii");
+  const specializationSelectValue =
+    document.querySelector(".specialization").value;
+  const ratingSelectValue = document.querySelector(".rating").value;
+  const areaSelectValue = document.querySelector(".area").value;
+
+  const params = new URLSearchParams(window.location.search);
+  params.set("specialization", specializationSelectValue);
+  params.set("servicearea", areaSelectValue);
+  params.set("rating", ratingSelectValue);
+
+  window.location.search = params;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+const params = new URLSearchParams(window.location.search);
+const specializationFilterValue = params.get("specialization");
+const servicaAreaFilterValue = params.get("servicearea");
+const ratingFilterValue = params.get("rating");
+
+function proffesionalsPageInit(data) {
+  let filteredData = [...data]; // Make a copy of the data array
+
+  if (specializationFilterValue) {
+    filteredData = filteredData.filter((item) =>
+      item.specialization.includes(specializationFilterValue)
+    );
+  }
+
+  if (servicaAreaFilterValue) {
+    filteredData = filteredData.filter((item) =>
+      item.serviceArea.includes(servicaAreaFilterValue)
+    );
+  }
+
+  if (ratingFilterValue) {
+    filteredData = filterByRating(ratingFilterValue, filteredData);
+  }
+
+  return filteredData;
+}
+
+function filterByRating(ratingFilterValue, dataArray) {
+  return dataArray.filter((item) => {
+    const starsSum = item.rating.totalStars;
+    const usersWhoRatedSum = item.rating.usersWhoRated;
+    const ratingSum = (starsSum / usersWhoRatedSum).toFixed(1);
+    return ratingSum >= ratingFilterValue;
+  });
 }
